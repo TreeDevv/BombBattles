cde24870333919cdf0017e9eecc542b0f0321590
Giovanni Hernandez <bartoart.1@gmail.com>
2026-06-22 15:37:05 -0400

    hunter is a chud

--- src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/Cola.lua
+++ src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/Cola.lua
@@ -1,3 +1,46 @@
 local EmoteEffect = require(script.Parent.Parent.EmoteEffect)
 
-return EmoteEffect.Create("Cola", { catalogOrder = 7 })
+
+
+local cola =  EmoteEffect.Create("Cola", { catalogOrder = 7 })
+
+
+
+function cola:Begin(_character: Model, runtime)
+	local root = runtime:GetRoot()
+	local rightArm = runtime:FindPart("Right Arm")
+
+	local cola = runtime:CloneVfxChildTo("BartoCola", runtime.character)
+	
+	local trackObject = runtime:PlayAnimation("Animation1", nil, nil)
+	if not trackObject then
+		return
+	end
+
+	runtime:TrackConnection(trackObject:GetMarkerReachedSignal("Drink"):Connect(function()
+		runtime:PlaySound(runtime:GetVfxChild("drink loop"), root)
+	end))
+	
+	runtime:TrackConnection(trackObject:GetMarkerReachedSignal("Open"):Connect(function()
+		runtime:PlaySound(runtime:GetVfxChild("soda pop"), root)
+	end))
+
+
+	runtime:MakeMotor(
+		rightArm or runtime.character,
+		"CouchM61",
+		rightArm,
+		cola :: BasePart?,
+		CFrame.new(0, -1.14, -0.14) * CFrame.Angles(math.rad(-90), 0, 0)
+	)
+end
+
+function cola:Finish(_character: Model, runtime)
+	local root = runtime:GetRoot()
+	local rightArm = runtime:FindPart("Left Arm")
+	runtime:DestroyChild(runtime.character, "BartoColaPower")
+end
+
+
+
+return cola

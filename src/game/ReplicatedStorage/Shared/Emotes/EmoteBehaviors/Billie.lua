5edd546770930eea0866cb4710b9b1ac3e99bde5
Giovanni Hernandez <bartoart.1@gmail.com>
2026-06-22 15:43:25 -0400

    d

--- src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/Billie.lua
+++ src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/Billie.lua
@@ -1,3 +1,17 @@
 local EmoteEffect = require(script.Parent.Parent.EmoteEffect)
 
-return EmoteEffect.Create("Billie", { catalogOrder = 1 })
+
+
+
+local Billie = EmoteEffect.Create("Billie", { catalogOrder = 1 })
+
+
+function Billie:Begin(_character: Model, runtime)
+	runtime:PlaySound(runtime:GetChild(runtime.vfxModule, "Music"), runtime:GetRoot())
+end
+
+function Billie:Finish(_character: Model, runtime)
+	runtime:DestroyChild(runtime:GetRoot(), "Music")
+end
+
+return Billie

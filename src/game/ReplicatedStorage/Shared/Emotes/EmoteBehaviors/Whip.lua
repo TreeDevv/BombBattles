2c260ffc451f1fc62a6142f381bc3f9675f24bfa
Giovanni Hernandez <bartoart.1@gmail.com>
2026-06-22 23:01:30 -0400

    d

--- src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/Whip.lua
+++ src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/Whip.lua
@@ -1,3 +1,16 @@
 local EmoteEffect = require(script.Parent.Parent.EmoteEffect)
 
-return EmoteEffect.Create("Whip", { catalogOrder = 34 })
+local Whip = EmoteEffect.Create("Whip", { catalogOrder = 34 })
+
+
+
+function Whip:Begin(_character: Model, runtime)
+	runtime:PlaySound(runtime:GetChild(runtime.vfxModule, "Music"), runtime:GetRoot())
+end
+
+function Whip:Finish(_character: Model, runtime)
+	runtime:DestroyChild(runtime:GetRoot(), "Music")
+end
+
+
+return Whip

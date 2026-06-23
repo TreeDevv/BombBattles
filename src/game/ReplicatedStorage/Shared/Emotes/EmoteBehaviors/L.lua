3c3923b18c1d082646415a8d8ed96f9624ae7738
Giovanni Hernandez <bartoart.1@gmail.com>
2026-06-22 23:12:26 -0400

    D

--- src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/L.lua
+++ src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/L.lua
@@ -1,3 +1,13 @@
 local EmoteEffect = require(script.Parent.Parent.EmoteEffect)
 
-return EmoteEffect.Create("L", { catalogOrder = 18 })
+local L = EmoteEffect.Create("L", { catalogOrder = 18 })
+function L:Begin(_character: Model, runtime)
+	runtime:PlaySound(runtime:GetChild(runtime.vfxModule, "Music"), runtime:GetRoot())
+end
+
+function L:Finish(_character: Model, runtime)
+	runtime:DestroyChild(runtime:GetRoot(), "Music")
+end
+
+
+return L

3eab3b0a94a581e780a50e91ea437c3657b86482
Giovanni Hernandez <bartoart.1@gmail.com>
2026-06-22 23:39:57 -0400

    d

--- src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/Default Dance.lua
+++ src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/Default Dance.lua
@@ -1,3 +1,15 @@
 local EmoteEffect = require(script.Parent.Parent.EmoteEffect)
 
-return EmoteEffect.Create("Default Dance", { catalogOrder = 10 })
+local DefaultDance= EmoteEffect.Create("Default Dance", { catalogOrder = 10 })
+
+
+function DefaultDance:Begin(_character: Model, runtime)
+	runtime:PlaySound(runtime:GetChild(runtime.vfxModule, "Music"), runtime:GetRoot())
+end
+
+function DefaultDance:Finish(_character: Model, runtime)
+	runtime:DestroyChild(runtime:GetRoot(), "Music")
+end
+
+
+return DefaultDance

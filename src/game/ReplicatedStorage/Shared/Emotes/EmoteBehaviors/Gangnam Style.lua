fbc85f3838efe3edc0b40425efc6e6ce9aa26aa1
Giovanni Hernandez <bartoart.1@gmail.com>
2026-06-22 22:33:45 -0400

    D

--- src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/Gangnam Style.lua
+++
@@ -1,3 +0,0 @@
-local EmoteEffect = require(script.Parent.Parent.EmoteEffect)
-
-return EmoteEffect.Create("Gangnam Style", { catalogOrder = 13 })
---
+++ src/game/ReplicatedStorage/Shared/Emotes/EmoteBehaviors/Ride the Pony.lua
@@ -0,0 +1,15 @@
+local EmoteEffect = require(script.Parent.Parent.EmoteEffect)
+
+local RidethePony = EmoteEffect.Create("Ride the Pony", { catalogOrder = 13 })
+
+
+
+function RidethePony:Begin(_character: Model, runtime)
+	runtime:PlaySound(runtime:GetChild(runtime.vfxModule, "Music"), runtime:GetRoot())
+end
+
+function RidethePony:Finish(_character: Model, runtime)
+	runtime:DestroyChild(runtime:GetRoot(), "Music")
+end
+
+return RidethePony
